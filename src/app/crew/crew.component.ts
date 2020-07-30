dca3d8e63b8875b18f05962714cdf6229036a94f
Doug Shook <dshook@wustl.edu>
2020-07-29 20:35:33 -0500

    studio 2 done

--- src/app/crew/crew.component.ts
+++ src/app/crew/crew.component.ts
@@ -7,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrewComponent implements OnInit {
 
+  memberBeingEdited: object = null;
+
   crew: object[] = [
     {name: "Eileen Collins", firstMission: false},
     {name: "Mae Jemison", firstMission: false},
-    {name: "Ellen Ochoa", firstMission: true}
+    {name: "Ellen Ochoa", firstMission: true},
+    {name: "Doug Shook", firstMission: false}
   ];
 
   constructor() { }
@@ -18,4 +21,36 @@ export class CrewComponent implements OnInit {
   ngOnInit() {
   }
 
+  add(memberName: string, isFirst: boolean) {
+    for(let i:number = 0; i < this.crew.length; i++) {
+      if(this.crew[i]['name'] === memberName) {
+        return;
+      }
+    }
+    this.crew.push({name: memberName, firstMission: isFirst});
+  }
+
+  remove(member: object) {
+    let index = this.crew.indexOf(member);
+    this.crew.splice(index, 1);
+  }
+
+  edit(member: object) {
+    this.memberBeingEdited = member;
+  }
+
+  save(name: string, member: object) {
+    this.memberBeingEdited = null;
+    if(name === "") {
+      alert("Please enter a name");
+      return;
+    }
+    for(let i:number = 0; i < this.crew.length; i++) {
+      if(this.crew[i]['name'] === name) {
+        alert("This crew member is already present");
+        return;
+      }
+    }
+    member['name'] = name;
+  }
 }
